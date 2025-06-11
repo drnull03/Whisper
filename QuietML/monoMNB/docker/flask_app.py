35e654badefd33c1d12db42bc0e8b24dba334f71
drnull03 <diaa.251202@gmail.com>
2025-06-11 12:24:08 +0200

    Finished Setting up monoMNB

--- QuietML/monoMNB/docker/flask_app.py
+++ QuietML/monoMNB/docker/flask_app.py
@@ -17,10 +17,15 @@ def predict():
 
     # Make the prediction using the spam detector model
     prediction = clf_loaded.predict([email_text])
-
+    probabilities = clf_loaded.predict_proba([email_text])
     # Return the result as JSON
     result = "Spam" if prediction == 1 else "Ham"
-    return jsonify({'prediction': result})
+    if prediction==1:
+        probabilities=probabilities[0][1]
+    else:
+        probabilities=probabilities[0][0]
+
+    return jsonify({'prediction': result,'probability': probabilities})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
